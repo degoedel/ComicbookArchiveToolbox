4317f950cfb28f70b36134bd9a0da871277bb79c
degoedel <damien.galban@gmail.com>
2019-05-04 15:03:50 +0200

    style: reident

--- ComicbookArchiveToolbox/ComicbookArchiveToolbox/ViewModels/HostViewModel.cs
+++ ComicbookArchiveToolbox/ComicbookArchiveToolbox/ViewModels/HostViewModel.cs
@@ -20,55 +20,52 @@ namespace ComicbookArchiveHost.ViewModels
   public class HostViewModel : BindableBase
   {
 
-	#region Attributes
-	private List<ICatPlugin> _plugins;
-	private IContainerExtension _container;
+	  #region Attributes
+	  private List<ICatPlugin> _plugins;
+	  private IContainerExtension _container;
     private IEventAggregator _eventAggregator;
 
-  #endregion Attributes
+    #endregion Attributes
 
     public CatViewModel DisplayedView { get; set; }
 
     #region Constructors
-  public HostViewModel(IContainerExtension container, IEventAggregator eventAggregator)
-  {
-		_container = container;
-    _eventAggregator = eventAggregator;
-    _eventAggregator.GetEvent<InterfaceLoadedEvent>().Subscribe(InitPluginsList);
+    public HostViewModel(IContainerExtension container, IEventAggregator eventAggregator)
+    {
+		  _container = container;
+      _eventAggregator = eventAggregator;
+      _eventAggregator.GetEvent<InterfaceLoadedEvent>().Subscribe(InitPluginsList);
+    }
+	  #endregion Constructors
 
-  }
-	#endregion Constructors
+	  public string HostTextContent => "This is the host from vm";
 
-	public string HostTextContent => "This is the host from vm";
-
-	private void InitPluginsList()
-	{
-		try
-		{
-			_plugins = new List<ICatPlugin>();
-			var type = typeof(ICatPlugin);
-			string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-			UriBuilder uri = new UriBuilder(codeBase);
-			string assemblyPath = Uri.UnescapeDataString(uri.Path);
-			DirectoryInfo installDir = new DirectoryInfo(System.IO.Path.GetDirectoryName(assemblyPath));
-			var listing = installDir.GetFiles("*CatPlugin*.dll").ToList();
-			List<string> pluginsPath = new List<string>();
-			foreach (FileInfo fi in listing)
-			{
-				pluginsPath.Add(fi.Name.Split('.')[1]);
-			}
-      List<ICatPlugin> plugins = new List<ICatPlugin>();
-      foreach (string s in pluginsPath)
-      {
-        plugins.Add(_container.Resolve<ICatPlugin>(s));
-      }
-
-		}
-		catch (Exception e)
-		{
-			Debug.WriteLine(e.Message);
-		}
-	}
-
+	  private void InitPluginsList()
+	  {
+		  try
+		  {
+			  _plugins = new List<ICatPlugin>();
+			  var type = typeof(ICatPlugin);
+			  string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+			  UriBuilder uri = new UriBuilder(codeBase);
+			  string assemblyPath = Uri.UnescapeDataString(uri.Path);
+			  DirectoryInfo installDir = new DirectoryInfo(System.IO.Path.GetDirectoryName(assemblyPath));
+			  var listing = installDir.GetFiles("*CatPlugin*.dll").ToList();
+			  List<string> pluginsPath = new List<string>();
+			  foreach (FileInfo fi in listing)
+			  {
+				  pluginsPath.Add(fi.Name.Split('.')[1]);
+			  }
+        List<ICatPlugin> plugins = new List<ICatPlugin>();
+        foreach (string s in pluginsPath)
+        {
+          plugins.Add(_container.Resolve<ICatPlugin>(s));
+        }
+		  }
+		  catch (Exception e)
+		  {
+			  Debug.WriteLine(e.Message);
+		  }
+	  }
   }
 }
