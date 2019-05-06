ee73b8bb207a77cd27fc3e0221a97a44a7ef3a85
degoedel <damien.galban@gmail.com>
2019-05-06 21:59:45 +0200

    chore: add the view of split plugin

--- ComicbookArchiveToolbox/ComicbookArchiveToolbox.SplitPlugin/ViewModels/SplitPluginViewModel.cs
+++ ComicbookArchiveToolbox/ComicbookArchiveToolbox.SplitPlugin/ViewModels/SplitPluginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using ComicbookArchiveToolbox.CommonTools;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,47 @@ namespace CatPlugin.Split.ViewModels
   public class SplitPluginViewModel : BindableBase
   {
     public string TextContent => "MySplitPluginText";
+
+    public string FileToSplit { get; set; }
+
+    public uint FileNb { get; set;}
+
+    private string _splitLog = "";
+    public string SplitLog
+    {
+      get { return _splitLog; }
+      set { SetProperty(ref _splitLog, value); }
+    }
+
+    public DelegateCommand BrowseFileCommand { get; private set; }
+    public DelegateCommand SplitCommand { get; private set; }
+
+    public SplitPluginViewModel()
+    {
+      BrowseFileCommand = new DelegateCommand(BrowseFile, CanExecute);
+      SplitCommand = new DelegateCommand(DoSplit, CanExecute);
+      SplitLog = "";
+    }
+
+    private void BrowseFile()
+    {
+      AddLogLine("Browse for file to split");
+    }
+
+    private void DoSplit()
+    {
+
+      AddLogLine("Split the file...");
+    }
+
+    private bool CanExecute()
+    {
+      return true;
+    }
+
+    private void AddLogLine(string line)
+    {
+      SplitLog += line + "\n";
+    }
   }
 }
