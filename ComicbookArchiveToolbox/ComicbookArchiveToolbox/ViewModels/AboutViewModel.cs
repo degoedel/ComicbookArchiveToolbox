d81d5a36a53b5af198ae1e4765d21a10493cfac8
Damien Galban <damien.galban@gmail.com>
2022-07-20 13:16:25 +0200

    chore: update about page

--- ComicbookArchiveToolbox/ComicbookArchiveToolbox/ViewModels/AboutViewModel.cs
+++ ComicbookArchiveToolbox/ComicbookArchiveToolbox/ViewModels/AboutViewModel.cs
@@ -9,10 +9,10 @@ namespace ComicbookArchiveToolbox.ViewModels
 {
 	public class AboutViewModel : BindableBase
 	{
-		public string AboutContent => "Comicbook Archive Toolbox v1.4 is a free open source extendable utility\n" +
+		public string AboutContent => "Comicbook Archive Toolbox v2.0 is a free open source extendable utility\n" +
 			"It allows to split and merge digital comic book archives such as cbr or cbz files\n" +
 			"and to edit their metadata.\n\n" +
-			"Comicbook Archive requires .NET 4.7.2 to work properly.\n\n" +
+			"Comicbook Archive requires .NET 6.0 to work properly.\n\n" +
 			"Comicbook Archive Toolbox depends on 7zip (redistributed) for compression/decompression of archives.\n\n" +
 			"Comicbook Archive Toolbox is developped by Damien Galban under MIT license.\n\n" +
 			"See https://degoedel.github.io/ComicbookArchiveToolbox/ for more details, source code and other dependencies";
