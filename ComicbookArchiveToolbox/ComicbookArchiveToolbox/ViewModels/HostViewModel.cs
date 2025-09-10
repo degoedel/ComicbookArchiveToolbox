9a8dfaeb405d129584b6db6c417c2ed2b42c7198
Damien Galban <damien.galban@gmail.com>
2025-09-10 09:51:55 +0200

    Revert "chore: Rework UI part 1"
    This reverts commit a4c8c4ae03d9efb9bcb5a877f8479db8cfe6ee6f.

--- ComicbookArchiveToolbox/ComicbookArchiveToolbox/ViewModels/HostViewModel.cs
+++ ComicbookArchiveToolbox/ComicbookArchiveToolbox/ViewModels/HostViewModel.cs
@@ -129,7 +129,7 @@ namespace ComicbookArchiveToolbox.ViewModels
 			get { return _hideLog; }
 			set
 			{
-				SetProperty<bool>(ref _hideLog, value);
+				SetProperty(ref _hideLog, value);
 			}
 		}
 
