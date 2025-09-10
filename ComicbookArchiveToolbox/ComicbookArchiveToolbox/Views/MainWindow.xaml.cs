93d439fdbfb3f5bc82db77ffb8d7d8797eabb572
Damien Galban <damien.galban@gmail.com>
2025-09-10 14:32:02 +0200

    chore: Experiment with themes

--- ComicbookArchiveToolbox/ComicbookArchiveToolbox/Views/MainWindow.xaml.cs
+++ ComicbookArchiveToolbox/ComicbookArchiveToolbox/Views/MainWindow.xaml.cs
@@ -3,16 +3,15 @@ using Prism.Navigation.Regions;
 
 namespace ComicbookArchiveToolbox.Views
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : MetroWindow
-  {
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : MetroWindow
+	{
 		public MainWindow(IRegionManager regionManager)
-    {
-      InitializeComponent();
-	    regionManager.RegisterViewWithRegion("HostRegion", typeof(HostView));
-	  }
-  }
+		{
+			this.InitializeComponent(); // Fully qualify to resolve ambiguity
+			regionManager.RegisterViewWithRegion("HostRegion", typeof(HostView));
+		}
+	}
 }
